a9c9e1976e9d47c5128e254d5f809fb90644d25f
gercelluciano <gercelluciano@hotmail.com>
2024-02-06 10:49:35 -0400

    todo3

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({forbidUnknownValues: false}));
-  await app.listen(3000, 'localhost');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
