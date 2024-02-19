8f2581f28fa30901599c0a7081847e7060dc4ec5
gercelluciano <gercelluciano@hotmail.com>
2024-02-19 16:17:59 -0400

    todo23

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -123,7 +123,7 @@ async create(files: Array<Express.Multer.File>,product: CreateProductsDto){
         const data1 ={
             tokens:listastrintoken,
             title:"NUEVA SEÑAL: "+newproduct.name+"CREADA",
-            body:"PUNTO 1:"+newproduct.price+"\n"+"PUNTO 2:"+newproduct.price1+"\n"+"PUNTO 3:"+newproduct.price2+"\n"+"SL:"+newproduct.sl+"\n"+
+            body:"PUNTO 1:"+newproduct.price+"\n"+"PUNTO 2:"+newproduct.price1+"\n"+"PUNTO 3:"+newproduct.price2+"\n"+"SL:"+newproduct.sl+"\n"
          }
          await PUSH(data1);
        }
