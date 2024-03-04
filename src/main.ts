e6d8f32beb9b13270f05070cd70a31f3f4f0b8b9
gercelluciano <gercelluciano@hotmail.com>
2024-03-04 13:43:38 -0400

    28

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
+  app.use(cors);
   app.useGlobalPipes(new ValidationPipe({forbidUnknownValues: false}));
   await app.listen(4000,'0.0.0.0' || 'localhost');
 }
